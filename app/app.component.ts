30e92e69e21e5f1611d50089ab34da3a958a881f
Swati <swatidubey28@gmail.com>
2016-12-18 09:36:02 +0530

    Initial commit with basic ui and functionality to add list bugs and display its count

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<div>
+              <bug-tracker></bug-tracker>
+            </div>
+            `,
+})
+export class AppComponent  { 
+  
+}
